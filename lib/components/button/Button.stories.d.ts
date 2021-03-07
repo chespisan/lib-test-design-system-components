7f8864f8b8c489e0b3edccd4f845edcb81ec6ac0
Sergio Sanchez <sergiosan2802@gmail.com>
2021-03-07 00:21:07 -0500

    first commit

---
+++ lib/components/button/Button.stories.d.ts
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { ButtonProps } from '.';
+declare const _default: Meta<import("@storybook/react/types-6-0").Args>;
+export default _default;
+export declare const Default: Story<ButtonProps>;
+export declare const DangerButton: Story<ButtonProps>;
+export declare const Ghost: Story<ButtonProps>;
+export declare const Secondary: Story<ButtonProps>;
+export declare const Disabled: Story<ButtonProps>;
+export declare const Loading: Story<ButtonProps>;
+export declare const WithIcon: Story<ButtonProps>;
+export declare const Sizes: () => JSX.Element;
+//# sourceMappingURL=Button.stories.d.ts.map
